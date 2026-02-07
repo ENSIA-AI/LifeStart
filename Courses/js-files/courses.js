79ff0664646a7da91998604188948f27cd157d82
aya <aya.laouadi@ensia.edu.dz>
2026-02-07 23:58:24 +0300

    Delete Courses directory

--- Courses/js-files/courses.js
+++
@@ -1,8 +0,0 @@
-const cards = document.querySelectorAll('.card');
-
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        const inner = card.querySelector('.card-inner');
-        inner.classList.toggle('flip');
-    });
-});
